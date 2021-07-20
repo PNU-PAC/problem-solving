30d615ca35f3326e3f22a24d9cb53fb62b5030ad
WH-Park <86655125+WH-Park@users.noreply.github.com>
2021-07-20 21:39:49 +0900

    FirstCompleteOfPrinterScript

--- C++/Printer.cpp
+++ C++/Printer.cpp
@@ -13,52 +13,70 @@ int main()
 	int location=2;
 
 	cout << "" << Printer(priorities,sizeof(priorities)/sizeof(int),location) << endl;
+
+	int priorities2[6]={1,1,9,1,1,1};
+	int location2=0;
+
+	cout << "" << Printer(priorities2,sizeof(priorities2)/sizeof(int),location2) << endl;
+
 	return 0;
 }//main()
 
-int Printer(int _priorities[],int _narray,int _location)
+int Printer(int _priorities[],const int _init_array_len,const int _location)
 {
-	int order=0;
-	int max = 0;
-	int nlarger = 0;
-	int nsame = 0;
-	int init_narray=_narray;
-
-	//vector<int> priorities;
+	int order=1;
+	int nelement=_init_array_len;
+	int location=_location;
 	queue<int> priorities;
 
-#if 1//_Debug_
-	for(int i=0;i<_narray;i++)
+	//vector<int> priorities;
+
+	//cout << "Initial array length : " << nelement << endl;
+	for(int i=0;i<nelement;i++)
 	{
 		//for test vector conversion
 		//priorities[i]=_priorities[i];
 
-		priorities.push<_priorities[i]>;
+		priorities.push(_priorities[i]);
 	}//i
-	_location = (_location+(_narray-nlager_or_same))%_narray;
-	init_narra -= nbigger;
-	
-	for(int ii=0;ii<init_narray;ii++)
+
+	for(int ii=0;ii<nelement;ii++)
 	{
+		//cout << "The Print that we want to know the order : "  << _priorities[_location] << endl;
 		if(priorities.front()>_priorities[_location])
 		{
-			nlager++;
 			priorities.pop();
-		}
-		else 
+			location -= location;
+			order++;
+		}else
 		{
-			priorities.push<priorities.front()>
+			priorities.push(priorities.front());
 			priorities.pop();
+			location = (location+(nelement-1))%nelement;
 		}
-
 	}//ii
-	for(int j=0;j<init_narray;j++)
+	//cout << "order before check number of same priorities tasks : " << order << endl;
+	//cout << "location before check number of same priorities tasks : " << location << endl;
+	for(int j=0;j<nelement-order+1;j++)
 	{
+		if(priorities.front()==_priorities[_location])
+		{
+			if(location == 0) return order;
+			else 
+			{
+				priorities.pop();
+				location -= 1;
+				//cout << "Changing location : "<< location << endl;
+				order++;
+				//cout << "Changing of order : " << order << endl;
+			}
+		}else
+		{
+			priorities.push(priorities.front());
+			priorities.pop();
+			location -= 1;
+		}
+	}//j
 
-	}
-	//_location+넘기는 횟수를 Divide by length of array!!
-#endif //_Debug_
-	
 	return order;
-
 }//Printer()
